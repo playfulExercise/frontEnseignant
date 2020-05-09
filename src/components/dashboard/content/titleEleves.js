3bfd48e33f8e75b02c1356cc85fe6899eb14a494
Clément <themega19@gmail.com>
2020-05-09 18:00:11 +0200

    fix header/titre eleves 2

--- src/components/dashboard/content/titleEleves.js
+++ src/components/dashboard/content/titleEleves.js
@@ -8,17 +8,17 @@ function TitleEleves() {
   );
 
   return (
-    <header className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 bg-white shadow flex content-start flex-wrap">
+    <header className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 bg-white shadow flex content-start flex-wrap">
       {redirectSubpageEleves && <Redirect to={redirectSubpageEleves} />}
-      <h1 className="text-3xl font-bold leading-tight text-gray-900">
+      <h1 className="text-3xl font-bold leading-tight text-gray-900 p-2">
         Gestion des eleves
       </h1>
-      <div class="w-30 ml-5 mr-5">
+      <div class="w-30 p-2">
         <Button onClick={() => setRedirectSubpageEleves("/eleves/all")}>
           Liste des eleves
         </Button>
       </div>
-      <div class="w-30">
+      <div class="w-30 p-2">
         <Button onClick={() => setRedirectSubpageEleves("/eleves/add")}>
           Ajouter un eleve
         </Button>
