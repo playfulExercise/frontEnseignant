01ade45d0441528bf9f05acc055dce9da49dfd3a
Clément <themega19@gmail.com>
2020-05-09 18:19:04 +0200

    Page monde commit 2

--- src/components/dashboard/content/monde.js
+++ src/components/dashboard/content/monde.js
@@ -4,17 +4,103 @@ import Button from "../../UI/Button";
 export default class Monde extends Component {
   render() {
     return (
-      <div className="flex content-start flex-wrap">
-        <div className="w-30 p-2">
-          <div className="text-center p-2">Anglais</div>
+      <div>
+        <div className="flex content-start flex-wrap justify-center">
+          <div className="w-1/3 p-2">
+            <div className="text-center text-2xl">Nombre de donjons</div>
+          </div>
+          <div className="1/5 p-1">
+            <Button typeButton>
+              <p className="text-2xl p-1">-</p>
+            </Button>
+          </div>
+          <div className="1/5 p-1">
+            <Button typeButton>
+              <p className="text-2xl p-1">+</p>
+            </Button>
+          </div>
+          <div className="1/5 p-2 text-2xl">0</div>
         </div>
-        <div class="w-30 p-2">
-          <Button>-</Button>
+        <div className="flex content-start flex-wrap justify-center">
+          <div className="w-1/3 p-2">
+            <div className="text-center text-2xl">Francais</div>
+          </div>
+          <div className="1/5 p-1">
+            <Button>
+              <p className="text-2xl p-1">-</p>
+            </Button>
+          </div>
+          <div className="1/5 p-1">
+            <Button>
+              <p className="text-2xl p-1">+</p>
+            </Button>
+          </div>
+          <div className="1/5 p-2 text-2xl">0</div>
         </div>
-        <div class="w-30 p-2">
-          <Button>+</Button>
+        <div className="flex content-start flex-wrap justify-center">
+          <div className="w-1/3 p-2">
+            <div className="text-center text-2xl">Maths</div>
+          </div>
+          <div className="1/5 p-1">
+            <Button>
+              <p className="text-2xl p-1">-</p>
+            </Button>
+          </div>
+          <div className="1/5 p-1">
+            <Button>
+              <p className="text-2xl p-1">+</p>
+            </Button>
+          </div>
+          <div className="1/5 p-2 text-2xl">0</div>
         </div>
-        <div class="w-30 p-2 text-2xl">0</div>
+        <div className="flex content-start flex-wrap justify-center">
+          <div className="w-1/3 p-2">
+            <div className="text-center text-2xl">Histoire</div>
+          </div>
+          <div className="1/5 p-1">
+            <Button>
+              <p className="text-2xl p-1">-</p>
+            </Button>
+          </div>
+          <div className="1/5 p-1">
+            <Button>
+              <p className="text-2xl p-1">+</p>
+            </Button>
+          </div>
+          <div className="1/5 p-2 text-2xl">0</div>
+        </div>
+        <div className="flex content-start flex-wrap justify-center">
+          <div className="w-1/3 p-2">
+            <div className="text-center text-2xl">Geographie</div>
+          </div>
+          <div className="1/5 p-1">
+            <Button>
+              <p className="text-2xl p-1">-</p>
+            </Button>
+          </div>
+          <div className="1/5 p-1">
+            <Button>
+              <p className="text-2xl p-1">+</p>
+            </Button>
+          </div>
+          <div className="1/5 p-2 text-2xl">0</div>
+        </div>
+        <div className="flex content-start flex-wrap justify-center">
+          <div className="w-1/3 p-2">
+            <div className="text-center text-2xl">Anglais</div>
+          </div>
+          <div className="1/5 p-1">
+            <Button>
+              <p className="text-2xl p-1">-</p>
+            </Button>
+          </div>
+          <div className="1/5 p-1">
+            <Button>
+              <p className="text-2xl p-1">+</p>
+            </Button>
+          </div>
+          <div className="1/5 p-2 text-2xl">0</div>
+        </div>
       </div>
     );
   }
