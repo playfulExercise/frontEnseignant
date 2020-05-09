d2e23d20dfc1551e64c09f2f6a198c72ac21f80f
Clément <themega19@gmail.com>
2020-05-09 17:58:29 +0200

    fix Header/Titre page eleves

--- src/components/dashboard/content/titleEleves.js
+++ src/components/dashboard/content/titleEleves.js
@@ -8,25 +8,20 @@ function TitleEleves() {
   );
 
   return (
-    <header className="bg-white shadow">
+    <header className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 bg-white shadow flex content-start flex-wrap">
       {redirectSubpageEleves && <Redirect to={redirectSubpageEleves} />}
-      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <span className="text-3xl font-bold leading-tight text-gray-900 w-full md:flex md:items-center">
-          Gestion des eleves
-          <Button
-            onClick={() => setRedirectSubpageEleves("/eleves/all")}
-            additionalClass="ml-5 mr-5"
-            setW="w-1/4"
-          >
-            Liste des eleves
-          </Button>
-          <Button
-            onClick={() => setRedirectSubpageEleves("/eleves/add")}
-            setW="w-1/4"
-          >
-            Ajouter un eleve
-          </Button>
-        </span>
+      <h1 className="text-3xl font-bold leading-tight text-gray-900">
+        Gestion des eleves
+      </h1>
+      <div class="w-30 ml-5 mr-5">
+        <Button onClick={() => setRedirectSubpageEleves("/eleves/all")}>
+          Liste des eleves
+        </Button>
+      </div>
+      <div class="w-30">
+        <Button onClick={() => setRedirectSubpageEleves("/eleves/add")}>
+          Ajouter un eleve
+        </Button>
       </div>
     </header>
   );
